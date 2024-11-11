6a079dace3823dee9f2ef4bb4ef219467a14dfe7
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-11-11 13:03:28 +0300

    подготовка проекта к работе

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5007,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.scss', '.js', '.jsx', '.ts', '.tsx'],
+  },
+});
